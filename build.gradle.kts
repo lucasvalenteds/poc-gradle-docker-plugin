f37caf2ae6d200364d64e6c4d23eb77061ddc714
Lucas <lucasvalenteds@gmail.com>
2020-10-07 22:24:32 -0300

    Managing container using Gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,14 @@
+import com.bmuschko.gradle.docker.tasks.container.DockerCreateContainer
+import com.bmuschko.gradle.docker.tasks.container.DockerStartContainer
+import com.bmuschko.gradle.docker.tasks.container.DockerStopContainer
+import com.bmuschko.gradle.docker.tasks.container.DockerRemoveContainer
+import com.bmuschko.gradle.docker.tasks.container.extras.DockerLivenessContainer
+import com.bmuschko.gradle.docker.tasks.image.DockerPullImage
+
 plugins {
     java
     application
+    id("com.bmuschko.docker-remote-api") version "6.6.1"
 }
 
 repositories {
@@ -19,11 +27,51 @@ configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_11
 }
 
+val serverPort = 5678
+
+val pullDockerImage by tasks.creating(DockerPullImage::class) {
+    image.set("hashicorp/http-echo:latest")
+}
+
+val createContainer by tasks.creating(DockerCreateContainer::class) {
+    dependsOn(pullDockerImage)
+    targetImageId(pullDockerImage.image.get())
+    hostConfig.portBindings.add("$serverPort:5678")
+    cmd.set(listOf("-text", "Hello world!"))
+}
+
+val startContainer by tasks.creating(DockerStartContainer::class) {
+    dependsOn(createContainer)
+    targetContainerId(createContainer.containerId)
+}
+
+val waitContainer by tasks.creating(DockerLivenessContainer::class.java) {
+    dependsOn(startContainer)
+    targetContainerId(createContainer.containerId)
+    livenessProbe("Server is listening")
+}
+
+val stopContainer by tasks.creating(DockerStopContainer::class) {
+    dependsOn(waitContainer)
+    targetContainerId(createContainer.containerId)
+}
+
+val removeContainer by tasks.creating(DockerRemoveContainer::class) {
+    dependsOn(stopContainer)
+    targetContainerId(createContainer.containerId)
+}
+
 tasks.withType<Test> {
+    dependsOn(waitContainer)
+    finalizedBy(removeContainer)
+    environment("SERVER_URL", "http://localhost:$serverPort")
+
     useJUnitPlatform()
-    environment("SERVER_URL", "http://localhost:5678")
 }
 
 tasks.withType<JavaExec> {
-    environment("SERVER_URL", "http://localhost:5678")
+    dependsOn(waitContainer)
+    finalizedBy(removeContainer)
+
+    environment("SERVER_URL", "http://localhost:$serverPort")
 }
