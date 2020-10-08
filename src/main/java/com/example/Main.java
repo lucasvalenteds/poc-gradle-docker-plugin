5a1e6a13e8ff22b9944cd218d60affcfff32d4e9
Lucas <lucasvalenteds@gmail.com>
2020-10-07 23:08:31 -0300

    Printing server URL

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -9,14 +9,16 @@ import java.net.http.HttpResponse;
 public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException {
+        String serverUrl = System.getenv("SERVER_URL");
         HttpResponse<String> response = HttpClient.newHttpClient().send(
-            HttpRequest.newBuilder().uri(URI.create(System.getenv("SERVER_URL"))).GET().build(),
+            HttpRequest.newBuilder().uri(URI.create(serverUrl)).GET().build(),
             HttpResponse.BodyHandlers.ofString()
         );
 
         assert response.statusCode() == 200;
         assert response.body().trim().equals("Hello world!");
 
+        System.out.println("Server URL: " + serverUrl);
         System.out.println("Status code: " + response.statusCode());
         System.out.println("Response body: " + response.body().trim());
     }
